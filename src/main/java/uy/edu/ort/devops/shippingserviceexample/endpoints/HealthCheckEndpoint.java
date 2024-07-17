bc29da3ef6b6f21f50a69e2e03a7e7703ded4c44
Thiago Schreck <thiago.schreck1@gmail.com>
2024-07-17 19:26:15 -0300

    emergencia

--- src/main/java/uy/edu/ort/devops/shippingserviceexample/endpoints/HealthCheckEndpoint.java
+++ src/main/java/uy/edu/ort/devops/shippingserviceexample/endpoints/HealthCheckEndpoint.java
@@ -1,4 +1,4 @@
-package uy.edu.ort.devops.paymentsserviceexample.endpoints;
+package uy.edu.ort.devops.shippingserviceexample.endpoints;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
